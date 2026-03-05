eb2ccc9b80d2df4081ce44c25b74c892794faf1d
Jorge <jjmarcant06@gmail.com>
2026-03-05 13:22:25 -0600

    Ram

--- simulador.py
+++ simulador.py
@@ -9,32 +9,42 @@ random.seed(RANDOM_SEED)
 
 # Parámetros iniciales
 INTERVALO = 10
-RAM_CAPACITY = 100
+RAM_CAPACITY = 5
 CPU_CAPACITY = 1
 INSTRUCCIONES_POR_TURNO = 3
 
-def proceso(env, nombre, cpu):
+def proceso(env, nombre, ram, cpu):
     print(nombre, "llega en tiempo", env.now)
-    
+
+    #  Solicita memoria
+    memoria = random.randint(1, 10)
+    print(nombre, "solicita", memoria, "de RAM")
+
+    yield ram.get(memoria)
+    print(nombre, "obtiene RAM en tiempo", env.now)
+
+    #  Total de instrucciones
     instrucciones = random.randint(1, 10)
     print(nombre, "tiene", instrucciones, "instrucciones")
-    
+
     while instrucciones > 0:
         with cpu.request() as req:
             yield req
-            
+
             ejecutar = min(INSTRUCCIONES_POR_TURNO, instrucciones)
-            
-            print(nombre, "usa CPU en", env.now, 
+
+            print(nombre, "usa CPU en", env.now,
                   "ejecutando", ejecutar, "instrucciones")
-            
+
             yield env.timeout(1)
-            
+
             instrucciones -= ejecutar
-            
-            print(nombre, "le quedan", instrucciones, "instrucciones")
-    
-    print(nombre, "termina en tiempo", env.now)
+
+            print(nombre, "le quedan", instrucciones)
+
+    #  Devuelve memoria al terminar
+    yield ram.put(memoria)
+    print(nombre, "termina y libera RAM en tiempo", env.now)
 # Crear el entorno 
 env = simpy.Environment()
 
@@ -46,8 +56,8 @@ print("Tiempo inicial:", env.now)
 print("RAM disponible:", ram.level)
 print("Capacidad CPU:", cpu.capacity)
 
-env.process(proceso(env, "Proceso 1", cpu))
-env.process(proceso(env, "Proceso 2", cpu))
+env.process(proceso(env, "Proceso 1", ram, cpu))
+env.process(proceso(env, "Proceso 2", ram, cpu))
 env.run()
 
 
