78464fde73c18d284fb0e3a86aea5ae6786139e4
Jorge <jjmarcant06@gmail.com>
2026-03-05 14:15:25 -0600

    Graficas

---
+++ app.py
@@ -0,0 +1,107 @@
+from tkinter import *
+import simulador 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Simulador de Procesos")
+        self.geometry("300x300")
+        self.bg="#B47E7E"
+        self.configure(bg=self.bg)
+        #  Labels e Inputs
+
+        Label(self, text="Procesos", bg=self.bg).pack()
+        self.entry_procesos = Entry(self)
+        self.entry_procesos.insert(0, "50")
+        self.entry_procesos.pack()
+
+        Label(self, text="Intervalo", bg=self.bg).pack()
+        self.entry_intervalo = Entry(self)
+        self.entry_intervalo.insert(0, "10")
+        self.entry_intervalo.pack()
+
+        Label(self, text="RAM", bg=self.bg).pack()
+        self.entry_ram = Entry(self)
+        self.entry_ram.insert(0, "100")
+        self.entry_ram.pack()
+
+        Label(self, text="CPUs", bg=self.bg).pack()
+        self.entry_cpu = Entry(self)
+        self.entry_cpu.insert(0, "1")
+        self.entry_cpu.pack()
+
+        Label(self, text="Instrucciones", bg=self.bg).pack()
+        self.entry_instrucciones = Entry(self)
+        self.entry_instrucciones.insert(0, "3")
+        self.entry_instrucciones.pack()
+
+        #  Botón
+        Button(self, text="Ejecutar", bg="#8B4513", command=self.ejecutar).pack(pady=5)
+        Button(self, text="Ejecutar Experimento", 
+       bg="#5A8F7B", 
+       command=self.ejecutar_experimento).pack(pady=5)
+
+        #  Resultado
+        self.label_resultado = Label(self, text="", bg=self.bg)
+        self.label_resultado.pack()
+    def ejecutar(self):# ejecuta solo lo que se pide 
+        try:
+            procesos = int(self.entry_procesos.get())
+            intervalo = float(self.entry_intervalo.get())
+            ram = int(self.entry_ram.get())
+            cpu = int(self.entry_cpu.get())
+            instrucciones_por_turno = int(self.entry_instrucciones.get())
+
+            prom, des = simulador.correr_simulacion(
+                procesos,
+                intervalo,
+                ram,
+                cpu,
+                instrucciones_por_turno
+            )
+
+            self.label_resultado.config(
+                text=f"Tiempo Promedio: {prom:.2f}\nDesviación: {des:.2f}"
+            )
+
+        except Exception as e:
+            self.label_resultado.config(text="Error en los datos")
+    def ejecutar_experimento(self):# ejecuta con 50,100,150,200 procesos y grafica el resultado
+        try:
+            intervalo = float(self.entry_intervalo.get())
+            ram = int(self.entry_ram.get())
+            cpu = int(self.entry_cpu.get())
+            procesos = int(self.entry_procesos.get())  
+            instrucciones = int(self.entry_instrucciones.get()) #Instrucciones por turno fijo para el experimento   CPU
+
+            cantidades, promedios = simulador.experimento(
+                intervalo,
+                ram,
+                cpu,
+                instrucciones
+            )
+
+            # Crear nueva ventana
+            ventana_grafica = Toplevel(self)
+            ventana_grafica.title("Resultados del Experimento")
+            ventana_grafica.geometry("600x400")
+
+            fig, ax = plt.subplots()
+            ax.plot(cantidades, promedios)
+            ax.set_xlabel("Número de procesos")
+            ax.set_ylabel("Tiempo promedio")
+            ax.set_title("Tiempo promedio vs Procesos")
+
+            canvas = FigureCanvasTkAgg(fig, master=ventana_grafica)
+            canvas.draw()
+            canvas.get_tk_widget().pack(fill=BOTH, expand=True)
+
+        except Exception as e:
+            print(e)
+            self.label_resultado.config(text="Error en experimento")
+            
+a=App()
+a.mainloop()
--- simulador.py
+++ simulador.py
@@ -1,63 +1,76 @@
 import simpy
 import random
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Semilla
 RANDOM_SEED = 42
-random.seed(RANDOM_SEED)
 
-# Parámetros iniciales
-INTERVALO = 10
-RAM_CAPACITY = 5
-CPU_CAPACITY = 1
-INSTRUCCIONES_POR_TURNO = 3
 
-def proceso(env, nombre, ram, cpu):
-    print(nombre, "llega en tiempo", env.now)
+def generador(env, ram, cpu, num_procesos, tiempos, intervalo, instrucciones_por_turno):
+    for i in range(num_procesos):
+        tiempo_espera = random.expovariate(1.0 / intervalo)
+        yield env.timeout(tiempo_espera)
+        env.process(
+            proceso(env, ram, cpu, tiempos, instrucciones_por_turno)
+        )
 
-    #  Solicita memoria
+
+def proceso(env, ram, cpu, tiempos, instrucciones_por_turno):
+    llegada = env.now
+
     memoria = random.randint(1, 10)
-    print(nombre, "solicita", memoria, "de RAM")
-
     yield ram.get(memoria)
-    print(nombre, "obtiene RAM en tiempo", env.now)
 
-    #  Total de instrucciones
     instrucciones = random.randint(1, 10)
-    print(nombre, "tiene", instrucciones, "instrucciones")
 
     while instrucciones > 0:
         with cpu.request() as req:
             yield req
-
-            ejecutar = min(INSTRUCCIONES_POR_TURNO, instrucciones)
-
-            print(nombre, "usa CPU en", env.now,
-                  "ejecutando", ejecutar, "instrucciones")
-
+            ejecutar = min(instrucciones_por_turno, instrucciones)
             yield env.timeout(1)
-
             instrucciones -= ejecutar
 
-            print(nombre, "le quedan", instrucciones)
-
-    #  Devuelve memoria al terminar
     yield ram.put(memoria)
-    print(nombre, "termina y libera RAM en tiempo", env.now)
-# Crear el entorno 
-env = simpy.Environment()
 
-# Crear recursos dentro del entorno
-ram = simpy.Container(env, init=RAM_CAPACITY, capacity=RAM_CAPACITY)
-cpu = simpy.Resource(env, capacity=CPU_CAPACITY)
-
-print("Tiempo inicial:", env.now)
-print("RAM disponible:", ram.level)
-print("Capacidad CPU:", cpu.capacity)
-
-env.process(proceso(env, "Proceso 1", ram, cpu))
-env.process(proceso(env, "Proceso 2", ram, cpu))
-env.run()
+    tiempo_total = env.now - llegada
+    tiempos.append(tiempo_total)
 
 
+def correr_simulacion(num_procesos, intervalo, ram_capacity, cpu_capacity, instrucciones_por_turno):
+
+    random.seed(RANDOM_SEED)
+
+    env = simpy.Environment()
+
+    ram = simpy.Container(env, init=ram_capacity, capacity=ram_capacity)
+    cpu = simpy.Resource(env, capacity=cpu_capacity)
+
+    tiempos = []
+
+    env.process(
+        generador(env, ram, cpu, num_procesos, tiempos, intervalo, instrucciones_por_turno)
+    )
+
+    env.run()
+
+    promedio = np.mean(tiempos)
+    desviacion = np.std(tiempos)
+
+    return promedio, desviacion
+
+
+def experimento(intervalo, ram, cpu, instrucciones_turno):
+
+    cantidades = [25, 50, 100, 150, 200]
+    promedios = []
+
+    for n in cantidades:
+        prom, _ = correr_simulacion(
+            n,
+            intervalo,
+            ram,
+            cpu,
+            instrucciones_turno
+        )
+        promedios.append(prom)
+
+    return cantidades, promedios
